d9404c77f188bde4aa3468c33b6c850163f35bda
Naveen-suryadevara <31907827+Naveen-suryadevara@users.noreply.github.com>
2017-09-28 19:20:08 -0500

    Add files via upload

---
+++ html.js
@@ -0,0 +1,38 @@
+//html link
+<
+!DOCTYPE html >
+    <
+    html >
+    <
+    body >
+    <
+    a href = "https://www.biography.com/people/steve-jobs-9354805" > this is a link < /a> <
+    /body> <
+    /html>
+
+/////html using elements
+<
+!DOCTYPE html >
+    <
+    html >
+
+    <
+    body >
+    <
+    div >
+    <
+    h1 >
+    Various types of porsche cars <
+    /h1> <
+    p >
+    <
+    span > Better in saftey and design. < /span> <
+    span > the coupe models are good looking < /span> <
+    /p> <
+    a href = "https://www.porsche.com/usa/" > click me < /a> <
+    img src = "7025.jpg"
+alt = "Porsche models"
+"> <
+/div> <
+/body> <
+/html>
